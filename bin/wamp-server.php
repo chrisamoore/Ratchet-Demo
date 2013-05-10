c1366b762b44cdcccee0102e567b12d7984614dc
Christopher A. Moore <chris.a.moore@gmail.com>
2013-05-10 11:25:39 -0700

    Thanks Igorw I forgot the Auto loader

--- bin/wamp-server.php
+++ bin/wamp-server.php
@@ -5,6 +5,8 @@ use Ratchet\Server\IoServer;
 use Ratchet\WebSocket\WsServer;
 use MyApp\Wamp;
 
+    require dirname(__DIR__) . '/vendor/autoload.php';
+
     $server = IoServer::factory(
         new WsServer(
             new WampServer(
